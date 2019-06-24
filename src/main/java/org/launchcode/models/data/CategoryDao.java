820e9903d6164174098fc4872c26520d24a821e3
Milena Davis <nilenadavis8808@gmail.com>
2019-06-24 09:51:57 -0500

    First studio done

--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -2,11 +2,12 @@ package org.launchcode.models.data;
 
 import org.launchcode.models.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
 import javax.transaction.Transactional;
 
-@Entity
+@Repository
 @Transactional
 public interface CategoryDao extends CrudRepository<Category, Integer> {
 }
